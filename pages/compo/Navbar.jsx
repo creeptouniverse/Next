23771e43b0873d66d38626bbd24394fc0bc1d83d
creeptouniverse <creeptouniverse@gmail.com>
2022-02-03 20:35:14 +0545

    Error fix

--- pages/compo/Navbar.jsx
+++ pages/compo/Navbar.jsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 export default function Navbar(){
     return(
         <>
-          <section classNameName="et-hero-tabs">
-            <div classNameName='bg-gray-500'>
+          <section className="et-hero-tabs">
+            <div className=''>
     <h1>STICKY SLIDER NAV</h1>
     <h3>Sliding content with sticky tab nav</h3></div>
-    <div className="et-hero-tabs-container bg-gray-500">
+    <div className="et-hero-tabs-container">
       <a className="et-hero-tab" href="#tab-es6">Information</a>
       <a className="et-hero-tab" href="#tab-flexbox">Members</a>
       <a className="et-hero-tab" href="#tab-react">Roadmap</a>
