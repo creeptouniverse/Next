a87243a68abacaf11764fde99b61665547449809
creeptouniverse <creeptouniverse@gmail.com>
2022-02-03 19:00:19 +0545

    Navbar

---
+++ pages/compo/Navbar.jsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+export default function Navbar(){
+    return(
+        <>
+     <nav>
+     <div className='place-content-center w-40 fixed top-0 left-0 right-0'>
+        <Image src={Logo} className=""></Image>
+        </div>
+      <div>
+        
+        <ul className="flex gap-5 bg-gray-800 text-white h-10 p-2">
+          <li><Link href='/' ><a>Home</a></Link></li>
+          <li><Link href='/Members'><a>Members</a></Link></li>
+          <li><Link href='/Roadmap'><a>Roadmap</a></Link></li>
+          <li><Link href='/About'><a>About</a></Link></li>
+        </ul>
+        <div className='place-content-center w-40 fixed top-0 left-0 right-0'>
+        </div>
+
+      </div>
+  
+    </nav>
+        </>
+    )
+}
