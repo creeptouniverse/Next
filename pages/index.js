a9ea518749593f43aa6f4f07bdd535fc932dd7e6
creeptouniverse <creeptouniverse@gmail.com>
2022-02-04 15:22:55 +0545

    image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Logo from './image/Logo.png'
 import Link from 'next/link'
 import Script from 'next/script'
 import Navbar from './compo/Navbar'
@@ -19,6 +18,8 @@ export default function Home() {
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+        <Image alt="Vercel logo" src="/Dark.png" width={100} height={100}/>
+  
       <Navbar />
 
   <main className="et-main">
@@ -28,7 +29,7 @@ export default function Home() {
     <About />
  <Contact />
   </main>
-  <div className='fixed flex shadow-lg hover:shadow-indigo-500/50 w-full bottom-0 p-2 m-2 rounded right-0 bg-black text-white'>
+  <div className='fixed flex shadow-lg hover:shadow-green-500/50 w-full bottom-0 p-2 rounded bg-black text-white'>
   <a className="et-hero-tab" href="#tab-es6">Information</a>
       <a className="et-hero-tab" href="#tab-flexbox">Members</a>
       <a className="et-hero-tab" href="#tab-react">Roadmap</a>
